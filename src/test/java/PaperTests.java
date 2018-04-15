8350f1c35d35b21502196c6947d64fa657039682
wilsontheory <wilson.brian90@gmail.com>
2018-04-15 13:06:12 +0700

    Initial commit with basic skeleton of project, chose maven for building

---
+++ src/test/java/PaperTests.java
@@ -0,0 +1,6 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class PaperTests {
+}
---
+++ src/test/java/PencilTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class PencilTests {
+
+    @Test
+    public void testOfTest(){
+        assertEquals(0, 0);
+    }
+}
